b8fa25ef92c00eea24a5c056f59e60979842ec6d
bartlomiej-potoniec <bartpot98@protonmail.com>
2024-04-28 11:38:11 +0200

    Create Domain logic

---
+++ HRLeaveManagement.Domain/LeaveRequest.cs
@@ -0,0 +1,23 @@
+﻿using HRLeaveManagement.Domain.Common;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HRLeaveManagement.Domain;
+
+public class LeaveRequest : BaseEntity
+{
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+
+    public int LeaveTypeId { get; set; }
+
+    [ForeignKey("LeaveTypeId")]
+    public LeaveType? LeaveType { get; set; }
+
+    public DateTime RequestedDate { get; set; }
+    public string? RequestedComments { get; set; }
+
+    public bool? Approved { get; set; }
+    public bool Canceled { get; set; }
+
+    public string RequestingEmployeeId { get; set; } = string.Empty;
+}
