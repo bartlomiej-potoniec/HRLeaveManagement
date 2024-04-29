0f88cc45b9048b224f4218f7a8d572bcccac56ac
bartlomiej-potoniec <bartpot98@protonmail.com>
2024-04-30 01:45:18 +0200

    Create first CQRS query and hanlder

---
+++ HRLeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypesQuery.cs
@@ -0,0 +1,6 @@
+﻿using HRLeaveManagement.Application.DTOs;
+using MediatR;
+
+namespace HRLeaveManagement.Application.Features.LeaveType.Queries;
+
+public record GetAllLeaveTypesQuery : IRequest<IEnumerable<LeaveTypeDTO>>;
---
+++ HRLeaveManagement.Application/Features/LeaveType/QueryHandlers/GetAllLeaveTypesQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using HRLeaveManagement.Application.Contracts.Persistence;
+using HRLeaveManagement.Application.DTOs;
+using HRLeaveManagement.Application.Features.LeaveType.Queries;
+using MediatR;
+using AutoMapper;
+
+namespace HRLeaveManagement.Application.Features.LeaveType.QueryHandlers;
+
+public class GetAllLeaveTypesQueryHandler(ILeaveTypeRepository repository, IMapper mapper) 
+    : IRequestHandler<GetAllLeaveTypesQuery, IEnumerable<LeaveTypeDTO>>
+{
+    private readonly ILeaveTypeRepository _repository = repository;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<IEnumerable<LeaveTypeDTO>> Handle(GetAllLeaveTypesQuery request,
+                                                        CancellationToken cancellationToken)
+    {
+        var leaveTypes = await _repository.GetAllAsync()
+            ?? throw new Exception();
+
+        var leaveTypeDtos = _mapper.Map<IEnumerable<LeaveTypeDTO>>(leaveTypes);
+
+        return leaveTypeDtos;
+    }
+}
