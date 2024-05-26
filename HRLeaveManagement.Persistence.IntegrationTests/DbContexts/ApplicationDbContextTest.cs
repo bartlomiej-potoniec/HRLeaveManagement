67de2c8aba46a4df428529ebb995c93f70ec406a
bartlomiej-potoniec <bartpot98@protonmail.com>
2024-05-26 09:11:44 +0200

    Init integration tests for Persistence layer

---
+++ HRLeaveManagement.Persistence.IntegrationTests/DbContexts/ApplicationDbContextTest.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using HRLeaveManagement.Domain;
+using HRLeaveManagement.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRLeaveManagement.Persistence.IntegrationTests.DbContexts;
+
+public class ApplicationDbContextTest
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public ApplicationDbContextTest()
+    {
+        var dbOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase("test_database")
+            .Options;
+
+        _dbContext = new ApplicationDbContext(dbOptions);
+    }
+
+    [Fact]
+    public async Task Save_SetDateCreatedValue()
+    {
+        // Arrange
+        var leaveType = new LeaveType { Id = 10, DefaultDays = 10, Name = "Test Vacation" };
+
+        // Act
+        await _dbContext.LeaveTypes.AddAsync(leaveType);
+        await _dbContext.SaveChangesAsync();
+
+        // Assert
+        leaveType.CreatedAt
+            .Should()
+            .NotBeNull();
+    }
+    
+    [Fact]
+    public async Task Save_SetDateModifiedValue()
+    {
+        // Arrange
+        var dateTime = DateTime.Now;
+        var leaveType = new LeaveType { Id = 11, DefaultDays = 10, Name = "Test Vacation" };
+
+        // Act
+        await _dbContext.LeaveTypes.AddAsync(leaveType);
+        await _dbContext.SaveChangesAsync();
+
+        // Assert
+        leaveType.ModifiedAt
+            .Should()
+            .NotBeNull();
+
+        leaveType.ModifiedAt
+            .Should()
+            .BeOnOrAfter(dateTime);
+    }
+}
