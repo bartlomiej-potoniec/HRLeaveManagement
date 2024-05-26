73ca0907088c96067cfa84b8056db1e1e52c4278
bartlomiej-potoniec <bartpot98@protonmail.com>
2024-05-26 09:10:32 +0200

    Init unit tests for Application layer

---
+++ HRLeaveManagement.Application.Tests/Features/LeaveType/QueryHandlers/GetAllLeaveTypesQueryHandlerTest.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using HRLeaveManagement.Application.Contracts.Infrastructure.Logging;
+using HRLeaveManagement.Application.Contracts.Persistence;
+using HRLeaveManagement.Application.DTOs;
+using HRLeaveManagement.Application.Features.LeaveType.Queries;
+using HRLeaveManagement.Application.Features.LeaveType.QueryHandlers;
+using HRLeaveManagement.Application.MappingProfiles;
+using HRLeaveManagement.Application.Tests.Mocks;
+using Moq;
+
+namespace HRLeaveManagement.Application.Tests.Features.LeaveType.QueryHandlers;
+
+public class GetAllLeaveTypesQueryHandlerTest
+{
+    private readonly Mock<ILeaveTypeRepository> _repositoryMock;
+    private readonly Mock<IAppLogger<GetAllLeaveTypesQueryHandler>> _loggerMock;
+    private readonly IMapper _mapper;
+
+    public GetAllLeaveTypesQueryHandlerTest()
+    {
+        _repositoryMock = LeaveTypeRepositoryMock.GetLeaveTypeLeaveTypeRepositoryMock();
+        _loggerMock = new Mock<IAppLogger<GetAllLeaveTypesQueryHandler>>();
+
+        var mapperConfig = new MapperConfiguration(c =>
+        {
+            c.AddProfile<LeaveTypeProfile>();
+        });
+
+        _mapper = mapperConfig.CreateMapper();
+    }
+
+    [Fact]
+    public async Task GetLeaveTypeListTest()
+    {
+        // Arrange
+        var handler = new GetAllLeaveTypesQueryHandler(
+            _repositoryMock.Object,
+            _mapper,
+            _loggerMock.Object
+        );
+
+        var query = new GetAllLeaveTypesQuery();
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Should().AllBeOfType<LeaveTypeDTO>();
+
+        result.Count().Should().Be(3);
+    }
+}
---
+++ HRLeaveManagement.Application.Tests/Mocks/LeaveTypeRepositoryMock.cs
@@ -0,0 +1,33 @@
+﻿using HRLeaveManagement.Domain;
+using HRLeaveManagement.Application.Contracts.Persistence;
+using Moq;
+
+namespace HRLeaveManagement.Application.Tests.Mocks;
+
+public class LeaveTypeRepositoryMock
+{
+    public static Mock<ILeaveTypeRepository> GetLeaveTypeLeaveTypeRepositoryMock()
+    {
+        List<LeaveType> leaveTypes = [
+            new() { Id = 1, DefaultDays = 10, Name = "Test Vacation" },
+            new() { Id = 2, DefaultDays = 15, Name = "Test Sick" },
+            new() { Id = 3, DefaultDays = 15, Name = "Test Maternity" },
+        ];
+
+        var mock = new Mock<ILeaveTypeRepository>();
+        
+        mock
+            .Setup(mock => mock.GetAllAsync())
+            .ReturnsAsync(leaveTypes);
+
+        mock
+            .Setup(mock => mock.CreateAsync(It.IsAny<LeaveType>()))
+            .ReturnsAsync((LeaveType leaveType) =>
+            {
+                leaveTypes.Add(leaveType);
+                return leaveType.Id;
+            });
+
+        return mock;
+    }
+}
